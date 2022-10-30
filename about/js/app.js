fac2f294705bcdcad728ab3382f0a78a60b93de3
dek adi <109329655+dekadii01@users.noreply.github.com>
2022-10-30 12:26:03 +0800

    Add files via upload

---
+++ about/js/app.js
@@ -0,0 +1,9 @@
+const ig = document.querySelector("#ig");
+ig.addEventListener("click", () => {
+    window.open("https://www.instagram.com/dekadii01/");
+})
+
+const link = document.querySelector("#linked");
+link.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/dekadii");
+})
