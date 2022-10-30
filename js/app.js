0672d69f9d773d3819fa5b761fe4f7fa15730f1d
dek adi <109329655+dekadii01@users.noreply.github.com>
2022-10-30 11:24:34 +0700

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const btnView = document.querySelector("#btn-view");
-btnView.addEventListener("click", () => {
-    window.open("https://dekadii01.github.io/laba.github.io/");
-})
-
-const btnView2 = document.querySelector("#btn-view2");
-btnView2.addEventListener("click", () => {
-    window.open("https://dekadii01.github.io/Porto-team-PWPB/");
-})
