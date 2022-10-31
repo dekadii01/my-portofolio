749384c82b83045235d62f5916522e0b86cd3393
dek adi <109329655+dekadii01@users.noreply.github.com>
2022-10-31 14:59:49 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const btnView = document.querySelector("#btn-view");
+btnView.addEventListener("click", () => {
+    window.open("https://dekadii01.github.io/laba.github.io/");
+})
+
+const btnCv = document.querySelector("#btn-cv");
+btnCv.addEventListener("click", () => {
+    window.open("source/cv.pdf");
+})
+
+const linked = document.querySelector("#linked");
+linked.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/dekadii");
+})
+
+const instagram = document.querySelector("#instagram");
+instagram.addEventListener("click", () => {
+    window.open("https://www.instagram.com/dekadii01/");
+})
+
+const btnGithub = document.querySelector("#btn-github");
+btnGithub.addEventListener("click", () => {
+    window.open("https://github.com/dekadii01");
+})
+
+const btnView2 = document.querySelector("#btn-view2");
+btnView2.addEventListener("click", () => {
+    window.open("https://dekadii01.github.io/Porto-team-PWPB/");
+})
